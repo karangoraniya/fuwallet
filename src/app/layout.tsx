1095e15af6319587755c4a4ae5027e4bc76cb615
Goraniya Karan <karangoraniya@gmail.com>
2024-10-08 00:39:25 +0530

    feat : Navbar removed 🚀

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster position="top-right" />
-        <Navbar />
+        {/* <Navbar /> */}
         <div>
           {children}
           <Script
